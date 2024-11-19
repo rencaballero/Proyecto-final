def94c9a4248518edd0af1f9063db1c9e329327b
rencaballero <renata.caballero@iteso.mx>
2024-11-18 18:20:03 -0600

    carpetas

---
+++ ProyectoWeb/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const path = require('path');
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Importa las rutas correctamente
+const authRoutes = require('./app/routes/router');
+const cuestionarioRoutes = require('./app/routes/cuestionario');
+const dashboardRoutes = require('./app/routes/dashboard');
+
+app.use(express.static('app/public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Conexión a MongoDB
+mongoose.connect('mongodb+srv://itesopruebas:itesopruebas@web2.uqcyj.mongodb.net/?retryWrites=true&w=majority&appName=WEB2');
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret: 'yourSecretKey',
+    resave: false,
+    saveUninitialized: false,
+}));
+
+// Usar las rutas con prefijos
+app.use('/auth', authRoutes);
+app.use('/cuestionario', cuestionarioRoutes);
+app.use('/dashboard', dashboardRoutes);
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
